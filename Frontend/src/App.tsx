456d39b254d79c296948a1dd06d2f38afb406a9b
Woisol-G <Woisol-G@outlook.com>
2025-01-25 13:53:01 +0800

    「✨新增 / feat」: 初步实现Sidebar

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
-import { SidebarProvider } from './components/ui/sidebar'
-import AppSideBar from './components/SideBar/AppSideBar'
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar'
+import SideBarApp from './components/SideBar/AppSideBar'
 
 function App() {
+  const [curDir, setCurDir] = useState('')
+  const [previewImg, setPreviewImg] = useState('')
+  function handleCurDirChange(curDir: string) {
+    setCurDir(curDir)
+  }
 
   return (
-    <SidebarProvider>
-      <AppSideBar />
-      <main className='bg-black'>
+    <SidebarProvider className=''>
+      <SideBarApp curDir={curDir} handleCurDirChange={handleCurDirChange} />
+      <main className='size-full p-1 flex flex-col items-center'>
+        <div className="w-full">
+          <SidebarTrigger className='size-10' />
+        </div>
+        <div className="relative w-full max-h-60">
+          <img className='size-52 max-h-52 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2' src={previewImg} alt="Preview" />
+        </div>
       </main>
     </SidebarProvider>
   )
--- Frontend/src/components/SideBar/AppSideBar.tsx
+++ Frontend/src/components/SideBar/AppSideBar.tsx
@@ -1,10 +1,36 @@
-import { Sidebar } from "lucide-react";
-import { SidebarContent } from "../ui/sidebar";
+// !wok真服了……导入了这个Sidebar导致一直显示的都只是svg……
+// import { Sidebar } from "lucide-react";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuItem } from "../ui/sidebar";
+import { Input } from "../ui/input";
+import { ChangeEvent, useState } from "react";
 
-export default function AppSideBar() {
+export default function SideBarApp({ curDir, handleCurDirChange }: { curDir: string, handleCurDirChange: (curDir: string) => void }) {
+	const [cwd, setCwd] = useState("")
+	const [dirs, setDirs] = useState(["Code", "Coding", "Game", "System"])
+	function handleCwdChange(e: ChangeEvent<HTMLInputElement>) {
+		// dtodo To Implement 调用go打开文件夹选择框
+		setCwd(e.target.value)
+
+	}
 	return (
 		<Sidebar>
-			<SidebarContent />
+			<SidebarContent className="w-60 h-full">
+				<SidebarGroup>
+					<SidebarHeader>
+						<Input id="dir_input" placeholder="Work Dir" type="text" value={cwd} onChange={handleCwdChange} multiple />
+					</SidebarHeader>
+					<SidebarGroupContent>
+						<SidebarGroupLabel>文件夹列表</SidebarGroupLabel>
+						<SidebarMenu>
+							{dirs.map((dir, index) => (
+								<SidebarMenuItem key={index} onClick={() => handleCurDirChange(dir)} className={`p-2 rounded-lg transition-all  ${curDir === dir ? "bg-primary text-white" : "hover:bg-secondary"}`}>
+									{dir}
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
 		</Sidebar>
 
 	)
